56bc27a18c4884e7f287e16b35d8cff03924d06e
AngelFFigueroaRivera <angfir@live.com.mx>
2020-04-15 02:50:07 -0500

    cambio html y css

--- mapas_covid.js
+++ mapas_covid.js
@@ -712,8 +712,8 @@ function graphimobile() {
     const vw = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
     const vh = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
 
-    var height  = 0.15*vh;
-    var width   = 0.10*vw;;
+    var height  = 0.55*vh;
+    var width   = 0.50*vw;;
     var margin = {top: 10, right: 20, bottom: 40, left: 30};
 
     width =     width - margin.left - margin.right;

f49a2b3465be090cd723cf08b9e7f204d7d33b9d
AngelFFigueroaRivera <angfir@live.com.mx>
2020-04-15 19:03:02 -0500

    graficas

--- mapas_covid.js
+++ mapas_covid.js
@@ -728,7 +728,7 @@ function graphicmovile() {
 
     var height  = 0.45*vh;
     var width   = 0.70*vw;;
-    var margin = {top: 10, right: 10, bottom: 40, left: 30};
+    var margin = {top: 10, right: 5, bottom: 40, left: 35};
 
     width =     width - margin.left - margin.right;
     height =    height - margin.top - margin.bottom;
